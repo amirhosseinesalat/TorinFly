58a12207dd7e695584df0e2973a40679b41c109c
amirhossein esalatfar <sltamirhosein@gmail.com>
2026-02-15 08:15:13 +0330

    add error page and layout

---
+++ pages/server-error.js
@@ -0,0 +1,22 @@
+import styles from "../styles/ServerError.module.css";
+import Image from "next/image";
+function servererror() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.right}>
+        <h1>اتصال با سرور برقرار نیست!</h1>
+        <h2>لطفا بعدا دوباره امتحان کنید.</h2>
+      </div>
+      <div className={styles.left}>
+        <Image
+          src="/images/ErrorLampRobot.png"
+          width={500}
+          height={500}
+          alt="error pic"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default servererror;
