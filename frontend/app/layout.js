85d1812cd93a26adff1e6329829cefc62995329a
amirhossein esalatfar <sltamirhosein@gmail.com>
2026-02-19 11:22:06 +0330

    responsive error page

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -2,6 +2,10 @@ import Layout from "../components/Layout";
 
 import "../styles/globals.css";
 
+export const metadata = {
+  title: "TorinFly",
+  description: "تور داخلی خارجی -تور های مسافرتی داخلی و خارجی با تورین فلای",
+};
 export default function RootLayout({ children }) {
   return (
     <html lang="fa" dir="rtl">
