8ce92a9333bfc0ebccebdd76d46f044465d0cc53
amirhossein esalatfar <sltamirhosein@gmail.com>
2026-02-20 17:12:14 +0330

    add styles for searchInputs

--- frontend/modules/Search.js
+++ frontend/modules/Search.js
@@ -1,6 +1,7 @@
 import styles from "../styles/Search.module.css";
 import { CiLocationOn } from "react-icons/ci";
 import { RiPinDistanceFill } from "react-icons/ri";
+import { MdOutlineDateRange } from "react-icons/md";
 function Search() {
   return (
     <div className={styles.container}>
@@ -13,12 +14,16 @@ function Search() {
       <div className={styles.inputLoc}>
         <CiLocationOn className={styles.icon} />
         <input placeholder="مبدا" />
+        <RiPinDistanceFill className={styles.icon2} />
         <input placeholder="مقصد" />
       </div>
       <div className={styles.date}>
+        <MdOutlineDateRange className={styles.dateIcon} />
         <input placeholder="تاریخ" />
       </div>
-      <button className={styles.search}>جسنجو</button>
+      <div className={styles.searchButton}>
+        <button className={styles.Button}>جستجو</button>
+      </div>
     </div>
   );
 }
