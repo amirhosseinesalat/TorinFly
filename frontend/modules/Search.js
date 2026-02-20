4ee13db46e832b1ad4b6e12a8096330c4987b488
amirhossein esalatfar <sltamirhosein@gmail.com>
2026-02-20 18:49:03 +0330

    responsive form search

--- frontend/modules/Search.js
+++ frontend/modules/Search.js
@@ -1,20 +1,21 @@
 import styles from "../styles/Search.module.css";
 
-
 import LocationSelect from "./LocationSelect";
 import DateSelect from "./DateSelect";
 function Search() {
   return (
-    <div className={styles.container}>
+    <>
       <div className={styles.title}>
         <h2>
           <span className={styles.span}>تورینو</span> برگزار کننده بهترین تور
           های داخلی و خارجی
         </h2>
       </div>
-      <LocationSelect />
-      <DateSelect />
-    </div>
+      <div className={styles.container}>
+        <LocationSelect />
+        <DateSelect />
+      </div>
+    </>
   );
 }
 
