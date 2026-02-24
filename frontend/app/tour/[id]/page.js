3be4d588830f97a0bf93d5916021677bee0db20c
amirhossein esalatfar <sltamirhosein@gmail.com>
2026-02-24 10:55:02 +0330

    update details page

--- frontend/app/tour/[id]/page.js
+++ frontend/app/tour/[id]/page.js
@@ -1,16 +1,21 @@
 import styles from "../../../styles/TourDetails.module.css";
-
+import { FaUserTie } from "react-icons/fa6";
+import { FaMap } from "react-icons/fa";
+import { PiMedal } from "react-icons/pi";
 async function TourDetails({ params }) {
   const res = await fetch(`http://localhost:6500/tour/${params.id}`, {
     cache: "no-store",
   });
 
   const tour = await res.json();
-
+  console.log(tour);
   if (!tour.id) {
     return <h2>توری پیدا نشد</h2>;
   }
+  const start = new Date(tour.startDate);
+  const end = new Date(tour.endDate);
 
+  const duration = Math.ceil((end - start) / (1000 * 60 * 60 * 24));
   return (
     <div className={styles.container}>
       <img src={tour.image} alt={tour.title} />
@@ -18,17 +23,34 @@ async function TourDetails({ params }) {
       <div className={styles.info}>
         <h1>{tour.title}</h1>
 
-        <p>
-          {tour.origin.name} → {tour.destination.name}
-        </p>
-
+        <p>{duration} روزه</p>
+        <div className={styles.details}>
+          <p>
+            {" "}
+            <FaUserTie />
+            تورلیدر از مبدا
+          </p>
+          <p>
+            {" "}
+            <FaMap />
+            برنامه سفر
+          </p>
+          <p>
+            {" "}
+            <PiMedal />
+            تضمین کیفیت
+          </p>
+        </div>
         <p>وسیله نقلیه: {tour.fleetVehicle}</p>
         <p>ظرفیت: {tour.capacity}</p>
-        <p>صندلی باقی‌مانده: {tour.availableSeats}</p>
-
-        <h3>{tour.price.toLocaleString()} تومان</h3>
-
-        <button className={styles.reserve}>ثبت و ادامه خرید</button>
+        <div className={styles.price}>
+          {" "}
+          <h3>
+            {tour.price.toLocaleString()}
+            <span style={{ fontSize: "16px", color: "gray" }}> تومان </span>
+          </h3>
+          <button className={styles.reserve}>رزرو و خرید</button>
+        </div>
       </div>
     </div>
   );
