8033f41423f4f0a891f69c50afc4d8bc5c01bc12
amirhossein esalatfar <sltamirhosein@gmail.com>
2026-03-10 16:42:54 +0330

    add header btn with style

--- frontend/components/ProfilePage.js
+++ frontend/components/ProfilePage.js
@@ -1,11 +1,29 @@
+"use client";
+import { useState } from "react";
 import styles from "../styles/ProfilePage.module.css";
 function ProfilePage() {
+  const [selected, setSelected] = useState("profile");
   return (
     <div>
       <div className={styles.active}>
-        <button className={styles.sign}>sign</button>
-        <button className={styles.sign}>sign</button>
-        <button className={styles.sign}>sign</button>
+        <button
+          className={selected === "profile" ? styles.select : ""}
+          onClick={() => setSelected("profile")}
+        >
+          پروفایل
+        </button>
+        <button
+          className={selected === "tours" ? styles.select : ""}
+          onClick={() => setSelected("tours")}
+        >
+          تور های من
+        </button>
+        <button
+          className={selected === "transaction" ? styles.select : ""}
+          onClick={() => setSelected("transaction")}
+        >
+          تراکنش ها
+        </button>
       </div>
     </div>
   );
