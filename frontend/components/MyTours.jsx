6835227e7db72f229d4f82f76efbab29a4ba2cd5
amirhossein esalatfar <sltamirhosein@gmail.com>
2026-04-30 10:54:51 +0430

    add menu to  My Tours Page

--- frontend/components/MyTours.jsx
+++ frontend/components/MyTours.jsx
@@ -1,6 +1,11 @@
+import UserMenu from "../modules/UserMenu";
 import styles from "../styles/MyTours.module.css";
 function MyTours() {
-  return <div className={styles.container}></div>;
+  return (
+    <div className={styles.container}>
+      <UserMenu />
+    </div>
+  );
 }
 
 export default MyTours;
