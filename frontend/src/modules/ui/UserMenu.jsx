a516b138f213cfadaf9904e99a342c7b13a72093
amirhossein esalatfar <sltamirhosein@gmail.com>
2026-05-05 11:31:42 +0430

    UI usermenu complated

--- frontend/src/modules/ui/UserMenu.jsx
+++ frontend/src/modules/ui/UserMenu.jsx
@@ -1,47 +1,35 @@
 "use client";
-
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import styles from "@/styles/UserMenu.module.css";
-import { useState } from "react";
 
 export default function UserMenu() {
-  const [selected, setSelected] = useState("profile");
+  const pathname = usePathname();
+
+  const isActive = (path) => pathname === path;
 
   return (
     <div className={styles.container}>
-      {" "}
-      <div className={styles.active}>
-        <Link
-          href="/profile"
-          passHref
-          className={`${styles.menuItem} ${selected === "profile" ? styles.select : ""}`}
-          onClick={() => {
-            setSelected("profile");
-          }}
-        >
-          پروفایل
-        </Link>
-        <Link
-          href="/profile/my-tours"
-          passHref
-          className={`${styles.menuItem} ${selected === "my-tours" ? styles.select2 : ""}`}
-          onClick={() => {
-            setSelected("my-tours");
-          }}
-        >
-          {" "}
-          تور های من
-        </Link>
-        <Link
-          href="/profile/transaction"
-          className={`${styles.menuItem} ${selected === "transaction" ? styles.select : ""}`}
-          onClick={() => {
-            setSelected("transaction");
-          }}
-        >
-          تراکنش ها
-        </Link>
-      </div>
+      <Link
+        href="/profile"
+        className={`${styles.menuItem} ${isActive("/profile") ? styles.active : ""}`}
+      >
+        پروفایل
+      </Link>
+
+      <Link
+        href="/profile/my-tours"
+        className={`${styles.menuItem} ${isActive("/profile/my-tours") ? styles.active : ""}`}
+      >
+        تور های من
+      </Link>
+
+      <Link
+        href="/profile/transaction"
+        className={`${styles.menuItem} ${isActive("/profile/transaction") ? styles.active : ""}`}
+      >
+        تراکنش ها
+      </Link>
     </div>
   );
 }
