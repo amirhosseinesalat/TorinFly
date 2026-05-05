f16add7c21e8f686163290a39f418a62fac4ff64
amirhossein esalatfar <sltamirhosein@gmail.com>
2026-05-05 11:24:49 +0430

    UI transcate page add to my project

--- frontend/src/components/TruncatePage.jsx
+++ frontend/src/components/TruncatePage.jsx
@@ -1,23 +1,30 @@
 import UserMenu from "@/modules/ui/UserMenu";
 import styles from "@/styles/TruncatePage.module.css";
 import { formatDate } from "@/utils/formatDate";
+
 function TruncatePage() {
+  const persianDate = formatDate(new Date());
+
   return (
     <div className={styles.container}>
       <UserMenu />
       <div className={styles.table}>
         <table>
           <thead>
-            <th>تاریخ و ساعت</th>
-            <th>مبلغ(تومان)</th>
-            <th>نوع تراکنش</th>
-            <th>شماره سفارش </th>
+            <tr>
+              <th>تاریخ و ساعت</th>
+              <th>مبلغ(تومان)</th>
+              <th>نوع تراکنش</th>
+              <th>شماره سفارش</th>
+            </tr>
           </thead>
           <tbody>
-            <td></td>
-            <td>dlf</td>
-            <td>dlf</td>
-            <td>dlf</td>
+            <tr>
+              <td>{persianDate}</td>
+              <td>12,000,000</td>
+              <td>ثبت نام در تور گردشگری</td>
+              <td>سفارش 12054902</td>
+            </tr>
           </tbody>
         </table>
       </div>
